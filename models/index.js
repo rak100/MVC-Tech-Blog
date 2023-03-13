a214d63591a5e5114e0ac21689589682acc5c007
rak10 <ahmadus10@icloud.com>
2023-03-13 16:46:50 +0500

    worked on models and css & js

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require("./User");
+const Post = require("./post.");
+
+User.hasMany(Post, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+module.exports = {
+  User,
+  Comment,
+  Post,
+};
